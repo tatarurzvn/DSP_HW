5ecea2a082bd949e53013435b7aec6eb07428b37
Razvan Tataru <tatarurzvn@hotmail.com>
2018-04-12 23:45:54 +0300

    added more ex and a status tracker

--- 3-14.py
+++ 3-14.py
@@ -9,5 +9,19 @@ Created on Wed Apr 11 15:24:27 2018
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = np.arange(0, 10, .01)
+def mySine(A, freq, phi, t):
+    return A * np.sin(2 * np.pi * freq * t + phi)
 
+# Test
+# t = np.arange(-5, 5, .05)
+# plt.plot(mySine(2, .5, 0, t) + 2)
+
+t = np.arange(0, 10, .01)
+q = mySine(10, 2, 0, t)
+
+f = open("3-14.txt", "w+")
+
+for x in q:
+    f.write("%f\n" % x)
+
+f.close()
